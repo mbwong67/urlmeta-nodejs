5749757b1b07e09c1a61e73ba8fb514db300a5b1
Boris Serebrov <serebrov@gmail.com>
2019-01-10 18:39:34 +0200

    Initial implementation

---
+++ app/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const getUrlMetadata = require('./urlmeta')
+
+const port = 3000
+const app = express()
+
+app.get('/url-parser', async (req, res, next) => {
+  const url = req.query.target;
+  if (!url) {
+      return next(new Error("Target url is not specified"));
+  }
+  try {
+    res.send(await getUrlMetadata(url))
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).json({'error': err.message})
+})
+
+app.listen(
+  port,
+  () => console.log(`Example app listening on port ${port}!`)
+)
---
+++ app/urlmeta.js
@@ -0,0 +1,145 @@
+const domino = require('domino')
+const fetch = require('node-fetch')
+/*
+ * Metadata parser, https://github.com/mozilla/page-metadata-parser
+ *
+ * The purpose of this library is to be able to find a consistent 
+ * set of metadata for any given web page. 
+ * Each individual kind of metadata has many rules which define 
+ * how it may be located.
+ *
+ * Metadata example:
+ * {
+ *   "description": "A Javascript library for parsing metadata on a web page. - mozilla/page-metadata-parser",
+ *   "icon":"https://github.githubassets.com/favicon.ico",
+ *   "image":"https://avatars0.githubusercontent.com/u/131524?s=400&v=4",
+ *   "title":"mozilla/page-metadata-parser",
+ *   "language":"en",
+ *   "type":"object",
+ *   "url":"https://github.com/mozilla/page-metadata-parser",
+ *   "provider":"GitHub"
+ * }
+ */
+const {getMetadata, metadataRuleSets} = require('page-metadata-parser')
+const urlParser = require('url')
+
+const MAX_ALT_IMAGES = 30
+
+// We extend standard metadata with og:video tag.
+var metadataRules = metadataRuleSets
+metadataRules["video"] = {
+  rules: [
+    ['meta[property="og:video:secure_url"]', element => element.getAttribute('content')],
+    ['meta[property="og:video:url"]', element => element.getAttribute('content')],
+    ['meta[property="og:video"]', element => element.getAttribute('content')],
+  ]
+}
+
+async function getUrlMetadata(targetUrl) {
+    const url = urlParser.parse(targetUrl)
+    const host = url.hostname
+
+    if (host.indexOf("youtube.com") !== -1 || host.indexOf("youtu.be") !== -1) {
+        return getYoutubeMetadata(url);
+    } else {
+        return getPageMetadata(url);
+    }
+}
+
+async function getYoutubeMetadata(targetUrl) {
+  var youtubeUrl = urlParser.parse("https://www.youtube.com/oembed")
+  youtubeUrl.query = {
+    "url": targetUrl.format(),
+    "format": "json"
+  }
+  const metadataUrl = youtubeUrl.format()
+
+  const response = await fetch(metadataUrl);
+  if (!response.ok) {
+    throw new Error("Can not fetch youtube data: " + response.statusText)
+  }
+  const metadata = await response.json()
+
+  return metadataResult({
+    "title":        metadata.title,
+    "url":          targetUrl.format(),
+    "image":        metadata.thumbnail_url,
+    "type":         metadata.type,
+    "siteName":     targetUrl.hostname,
+  });
+}
+
+async function getPageMetadata(url) {
+  // Fetch the URL and get page text.
+  const response = await fetch(url.format())
+  if (!response.ok) {
+    throw new Error("Can not fetch page: " + response.statusText)
+  }
+  const html = await response.text()
+
+  // Parse page text with domino.
+  const doc = domino.createWindow(html).document
+
+  // Extract metadata.
+  const metadata = getMetadata(doc, url, metadataRules)
+  const hasOgTags = doc.querySelectorAll('meta[property="og:title"]').length !== 0
+
+  var data = {
+    "title":        metadata.title,
+    "description":  metadata.description,
+    "url":          metadata.url.format(),
+    "image":        metadata.image,
+    "type":         metadata.type,
+    "locale":       metadata.language,
+    "video":        metadata.video,
+    "hasOgTags":    hasOgTags,
+    "siteName":     url.hostname,
+    "altImages":    hasOgTags ? [] : getDocumentImages(doc, url),
+  }
+  // TODO: looks like "undefined" values are excluded from output
+  // along with the keys, why?
+  console.log(data);
+  return metadataResult(data);
+}
+
+function imageThatShouldBeExcluded(imageSource) {
+   return imageSource.startsWith("https://www.facebook.com/tr?")
+}
+
+function getDocumentImages(doc, docUrl) {
+  const images = doc.querySelectorAll("img")
+  const count = Math.min(images.length, MAX_ALT_IMAGES)
+
+  var result = [];
+  for (var i = 0; i < count; i++) {
+    imageSource = images[i].src
+    if(!imageSource || imageThatShouldBeExcluded(imageSource)) {
+      continue
+    }
+    // TODO: test with "bad" image links.
+    var imageUrl = urlParser.parse(imageSource)
+    if (!imageUrl.host) {
+      result.push(docUrl.resolve(imageUrl))
+    } else {
+      result.push(docUrl.format())
+    }
+  }
+  return result
+}
+
+function metadataResult(metadata) {
+  return Object.assign ({
+    "title":        "",
+    "description":  "",
+    "url":          "",
+    "image":        "",
+    "type":         "",
+    "locale":       "",
+    "video":        "",
+    "hasOgTags":    false,
+    "siteName":     "",
+    "altImages":    [],
+  }, metadata);
+}
+
+module.exports = getUrlMetadata
